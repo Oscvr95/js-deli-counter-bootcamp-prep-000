08eac4e87a347589da77637fa763c1b1423c0891
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-10 23:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine() {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-        return `The line is currently: ${i}. ${katzDeliLine}`
+        return `The line is currently: ${katzDeliLine}`
   }
 
   }
