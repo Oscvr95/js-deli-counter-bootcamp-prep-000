03bf317524acaae96a6caa53050556e53cefca8f
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-11 02:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(newArray) {
+function currentLine(katzDeliLine) {
 
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-
+      
   }
 }
